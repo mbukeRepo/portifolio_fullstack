d71b767da9342be06710799671c4846a53492940
mbukeRepo <mbuke404@gmail.com>
2022-05-29 23:46:18 +0200

    changed app.js

--- app.js
+++ app.js
@@ -13,23 +13,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('605e1256cd5a74a212578e2a')
-    .then(user => {
-      req.user = new User(user.name,user.email,user._id,user.cart) ;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
